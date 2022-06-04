db5fd00de9864789403387ba8d52d103205cb475
gauravdhabale30 <96278811+gauravdhabale30@users.noreply.github.com>
2022-06-04 14:32:44 +0530

    Add files via upload
    html using jsx & without using jsx

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,15 @@
 import React from "react";
 
 const Hello = () => {
-  return (
-    <div>
-      <h1>Hello Gaurav</h1>
-    </div>
+  // return (
+  //   <div className='dummyClass'>
+  //     <h1>Hello Gaurav</h1>
+  //   </div>
+  // );
+  return React.createElement(
+    "div",
+    { id: "hello", class: "dummyclass" },
+    React.createElement("h1", null, "Hello Gaurav")
   );
 };
 
