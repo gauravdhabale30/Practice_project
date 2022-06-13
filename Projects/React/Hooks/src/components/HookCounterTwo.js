ee9c6c53e9cb5cda3fdcc44990194915d294d599
gauravdhabale30 <96278811+gauravdhabale30@users.noreply.github.com>
2022-06-13 13:09:37 +0530

    Add files via upload
    Hooks counter+5 code done

---
+++ Projects/React/Hooks/src/components/HookCounterTwo.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function HookCounterTwo() {
+  const initialCount = 0;
+  const [count, setCount] = useState(initialCount);
+
+  const incrementFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  };
+  return (
+    <div>
+      <div>count:{count}</div>
+      <div>
+        <button onClick={() => setCount(initialCount)}>Reset</button>
+        <button onClick={() => setCount((prevCount) => count + 1)}>
+          Increment
+        </button>
+        <button onClick={() => setCount((prevCount) => count - 1)}>
+          Decrement
+        </button>
+        <button onClick={incrementFive}>Increment 5</button>
+      </div>
+    </div>
+  );
+}
+
+export default HookCounterTwo;
