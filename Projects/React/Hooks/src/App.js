4ba87565994921f6139ff1e4906311bf4720cb4c
gauravdhabale30 <96278811+gauravdhabale30@users.noreply.github.com>
2022-06-15 12:21:37 +0530

    Add files via upload
    useEffect intervalHookCounter component done

--- Projects/React/Hooks/src/App.js
+++ Projects/React/Hooks/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Classcounter from "./components/useEffect/Classcounter";
 import HookCounterOne from "./components/useEffect/HookCounterOne";
 import HookMouse from "./components/useEffect/HookMouse";
+import IntervalHookCounter from "./components/useEffect/IntervalHookCounter";
 import MouseContainer from "./components/useEffect/MouseContainer";
 import ClassCounter from "./components/useState/ClassCounter";
 import HookCounter from "./components/useState/HookCounter";
@@ -13,7 +14,8 @@ import HookCounterTwo from "./components/useState/HookCounterTwo";
 function App() {
   return (
     <div className="App">
-      <MouseContainer />
+      <IntervalHookCounter />
+      {/* <MouseContainer /> */}
       {/* <HookMouse /> */}
       {/* <HookCounterOne /> */}
       {/* <Classcounter /> */}
---
+++ Projects/React/Hooks/src/components/useEffect/IntervalHookCounter.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+function IntervalHookCounter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(tick, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [count]);
+  const tick = () => {
+    setCount(count + 1);
+  };
+
+  return <h1>{count}</h1>;
+}
+
+export default IntervalHookCounter;
