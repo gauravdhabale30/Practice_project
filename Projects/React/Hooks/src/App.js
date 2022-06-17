2ac4214e33dc71e197526116fb0ed1f02eb87d6d
gauravdhabale30 <96278811+gauravdhabale30@users.noreply.github.com>
2022-06-17 13:38:32 +0530

    Add files via upload
    useReducer with useContext component done

--- Projects/React/Hooks/src/App.js
+++ Projects/React/Hooks/src/App.js
@@ -1,6 +1,7 @@
 //import logo from "./logo.svg";
+import React, { useReducer } from "react";
 import "./App.css";
-import ComponentA from "./components/useContext/ComponentA";
+// import ComponentA from "./components/useContext/ComponentA";
 import FunCompoA from "./components/useContext/FunCompoA";
 import { UserProvider } from "./components/useContext/UserContext";
 import Classcounter from "./components/useEffect/Classcounter";
@@ -11,6 +12,9 @@ import HookCounterOne from "./components/useEffect/HookCounterOne";
 import HookMouse from "./components/useEffect/HookMouse";
 import IntervalHookCounter from "./components/useEffect/IntervalHookCounter";
 import MouseContainer from "./components/useEffect/MouseContainer";
+import ComponentB from "./components/useReducer/ComponentB";
+import ComponentA from "./components/useReducer/ComponentA";
+import ComponentC from "./components/useReducer/ComponentC";
 import CounterOne from "./components/useReducer/CounterOne";
 import CounterThree from "./components/useReducer/CounterThree";
 import CounterTwo from "./components/useReducer/CounterTwo";
@@ -20,14 +24,38 @@ import HookCounterFour from "./components/useState/HookCounterFour";
 import HookCounterThree from "./components/useState/HookCounterThree";
 import HookCounterTwo from "./components/useState/HookCounterTwo";
 
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+export const CountContext = React.createContext();
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState);
   return (
     <div className="App">
       {/*-------------------------- useReducer hooks --------------------------------- */}
-      <CounterThree />
+      <CountContext.Provider
+        value={{ countState: count, countDispatch: dispatch }}
+      >
+        <div>
+          count {count}
+          <ComponentA />
+          <ComponentB />
+          <ComponentC />
+        </div>
+      </CountContext.Provider>
+      {/* <CounterThree /> */}
       {/* <CounterTwo /> */}
       {/* <CounterOne /> */}
-
       {/*-------------------------- useContext hooks --------------------------------- */}
       {/* <FunCompoA /> */}
       {/* <UserProvider value="Gaurav">
