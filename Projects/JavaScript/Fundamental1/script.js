1d25d351247f0235c22be6dd2a7b85b43dfed7e4
gauravdhabale30 <96278811+gauravdhabale30@users.noreply.github.com>
2022-06-22 12:27:11 +0530

    Add files via upload
    Strings and Template Literals completed

--- Projects/JavaScript/Fundamental1/script.js
+++ Projects/JavaScript/Fundamental1/script.js
@@ -148,7 +148,7 @@ GOOD LUCK 😀
 // const heightMark = 1.69;
 // const massJohn = 92;
 // const heightJohn = 1.95;
-
+/*
 const massMark = 95;
 const heightMark = 1.88;
 const massJohn = 85;
@@ -159,3 +159,29 @@ const BMIJohn = massJohn / (heightJohn * heightJohn);
 const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn, markHigherBMI);
+
+*/
+
+////////////////////////////////////
+// Strings and Template Literals
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " year old " + job + "!";
+console.log(jonas);
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log("String with \n\
+multiple \n\
+lines");
+
+console.log(`String
+multiple
+lines`);
