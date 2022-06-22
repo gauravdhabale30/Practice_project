a9cb7d97cdbdaad0d475b652fa0cbbc80383ad8a
gauravdhabale30 <96278811+gauravdhabale30@users.noreply.github.com>
2022-06-22 10:48:03 +0530

    Add files via upload
    Data type added in project

--- Projects/JavaScript/Fundamental1/script.js
+++ Projects/JavaScript/Fundamental1/script.js
@@ -1,5 +1,5 @@
 // Linking a JavaScript File
-let js = "amazing";
+/* let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 ////////////////////////////////////
@@ -26,4 +26,26 @@ let myCurrentJob = "Teacher";
 let job1 = "programmer";
 let job2 = "teacher";
 
-console.log(myFirstJob);
+console.log(myFirstJob);  */
+
+////////////////////////////////////
+// Data Types
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
